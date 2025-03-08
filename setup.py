226da7b15400223a9d987da48f529dd1f8f8dcd3
jkpraveena <praveenakamalakrishnan@gmail.com>
2025-03-08 13:14:09 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='J K Praveena',
+    author_email='jkpraveena2006@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
